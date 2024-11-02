f1ff09f05da680a7571b7d16ba119b9e82cc9885
SargiSimonyan <sargissimonyan2@gmail.com>
2024-11-02 22:55:22 +0400

    to clean the code insaid layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,41 +3,6 @@ import { data } from "../data/data"
 import request from "../request"
 export default function Layout () {
 
-  // const [BlogListData, setBlogListData] = useState(null);
-  // const [originalData, setOriginalData] = useState(null);
-
-  // useEffect(() => {
-  //   request(`https://cloud.codesupply.co/endpoint/react/data.json`)
-  //     .then((home) => {
-  //       setBlogListData(home);
-  //       setOriginalData(home);
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     })
-  // }, []);
-
-  // const setCurrentStop = (inputValue) => {
-  //   if (inputValue === undefined) {
-  //     return;
-  //   }
-  //   if (inputValue === '') {
-  //     setBlogListData(originalData);
-  //   } else {
-  //     if (originalData) {
-  //       const results = originalData.filter(blog =>
-  //         (blog.title && blog.title.toLowerCase().includes(inputValue.toLowerCase())) ||
-  //         (blog.text && blog.text.toLowerCase().includes(inputValue.toLowerCase()))
-  //       );
-  //       setBlogListData(results);
-  //     }
-  //   }
-  // };
-
-  // if(!BlogListData){
-  //   return (<div>Loading...</div> )}
-
-
   const [BlogListData, setBlogListData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
